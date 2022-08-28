bdc2ad380042593e8b696c9e784101bf3ef20911
liuyixi <liuyixi.lyx@alibaba-inc.com>
2022-08-28 19:02:00 +0800

    add comment

--- src/core/infer_action.rs
+++ src/core/infer_action.rs
@@ -4,8 +4,8 @@ use super::{env::Env, exp::{Type, Exp, Literal}, scheme::Scheme, inferable::CanI
 
 pub fn do_type_infer(default_env:& Env, exp:& Exp) -> Type {
     let (s, t) = infer(&default_env, &exp);
-    println!("infer finish, substitutions: {:?}", s);
-    return t;
+    println!("infer finish, substitutions: {:?} type: {:?}", s, t);
+    return t.apply(&s);
 }
 
 pub fn generalize(env: Env, t: Type) -> Scheme {
@@ -23,13 +23,15 @@ pub fn instantiated(scheme: Scheme) -> Type {
         .map(|_| create_new_type_var(&String::from("a")))
         .collect();
     let substitute = Substitution::new(a.into_iter().zip(b.into_iter()).collect());
+
+    println!("instantiated, sub: {:?}, type: {}", substitute, scheme.get_type());
     return scheme.get_type().apply(&substitute);
 }
 
 pub fn unification(left:& Type, right:& Type) -> Substitution {
     match (left, right) {
         (Type::TFun(t1_left, t2_left), Type::TFun(t1_right, t2_right)) => {
-            let u1 = unification(t1_left, &t2_left);
+            let u1 = unification(t1_left, t2_left);
             let u2 = unification(t1_right, t2_right);
             return u1.compose_substitution(&u2);
         },
@@ -54,19 +56,20 @@ pub fn unification(left:& Type, right:& Type) -> Substitution {
 pub fn infer(env:& Env, exp:& Exp) -> (Substitution, Type) {
     match exp {
         Exp::EVar(x) => {
-            return (Substitution::empty(), instantiated(env.find(&x)));
+            return (Substitution::empty(), instantiated(env.find(&x))); 
         },
-        Exp::EApp(func, param) => {
+        Exp::EApp(func, param) => {                                     
             let return_type = create_new_type_var(&String::from("a"));
             let (s1, type_of_func) = infer(&env, &func);
             let (s2, type_of_param) = infer(&env.apply(&s1), &param);
 
             println!("App infer info: return_type is {}, func_type is {}, param_type is{}", return_type, type_of_func, type_of_param);
             
+            // x -> x x x 1
             let s3 = unification(&type_of_func.apply(&s2), &Type::TFun(Box::new(type_of_param), Box::new(return_type.clone()))); 
 
             println!("App subt: s1 is {:?} s2 is {:?} s3 is {:?}", s1, s2, s3);
-            return (s3.compose_substitution(&s2).compose_substitution(&s1), return_type.apply(&s3));
+            return (s3.compose_substitution(&s2).compose_substitution(&s1), return_type.apply(&s3)); 
         },
         Exp::EAbs(parm_name, func_body) => {
             let param_type = create_new_type_var(&String::from("a"));
@@ -85,6 +88,7 @@ pub fn infer(env:& Env, exp:& Exp) -> (Substitution, Type) {
         Exp::ELet(x, e1, e2) => {
             let (s1, e1_type) = infer(env, e1);
             
+            println!("Let: e1_type is {}", e1_type.to_string());
             let generic_type_for_e1 = generalize(env.apply(&s1), e1_type);
             let mut var_type_item = HashMap::new();
             var_type_item.insert((*x).clone(), generic_type_for_e1);
@@ -101,7 +105,6 @@ pub fn infer(env:& Env, exp:& Exp) -> (Substitution, Type) {
             return infer_literal(&lit);
         },
     }
-
 }
 
 pub fn infer_literal(lit:& Literal) -> (Substitution, Type) {
--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,66 @@ use std::collections::HashMap;
 use type_infer_sys::core::{exp::{Exp, Type, Literal}, env::Env, infer_action::do_type_infer, scheme::Scheme};
 
 fn main() {    
+    // 预设环境
     let mut map:HashMap<String, Scheme> = HashMap::new();
-    // map.insert(String::from("x"), Scheme::new([].to_vec(), Type::TInt));
+    // map.insert(String::from("x"), Scheme::new(["x".to_string()].to_vec(), Type::TVar("x".to_string())));
+    // map.insert(String::from("y"), Scheme::new(["y".to_string()].to_vec(), Type::TVar("y".to_string())));
     let default_env = Env::new(map);
     
-    // test_app(&default_env);
-    test_let_id_x_x_id_2(&default_env);
-    // test_let_id_x_let_y_x_x_id(&default_env);
+    // 测试函数
+    test_two(&default_env);
 }
 
-fn test_app(default_env:& Env) {
-    let res = Exp::EVar("x".to_string());
-    let e0: Exp = Exp::EAbs("x".to_string(), &res);
+// f: y -> y, f(true)
+fn test_one(default_env:& Env) {
+    let res = Exp::EVar("y".to_string());
+    let e0_1: Exp = Exp::EAbs("y".to_string(), &res);
+
+    let bool_p = Exp::ELit(Literal::LBool(true));
+    let e0: Exp = Exp::EApp(&e0_1, &bool_p);
     let t = do_type_infer(default_env, &e0);
-    println!("{:?}", t);
+    println!("{}", t.to_string());
 }
 
-fn test_let_id_x_x_id(default_env:& Env) {
+// f: x -> x(x(1))
+fn test_two(default_env:& Env) {
+    let x_1 = Exp::EVar("x".to_string());
+    let p_0 = Exp::ELit(Literal::LInt(1));
+    let app_0 = Exp::EApp(&x_1, &p_0);
+
+    let x_2 = Exp::EVar("x".to_string());
+    let app_0 = Exp::EApp(&x_2, &app_0);
+
+    let abs_1 = Exp::EAbs("x".to_string(), &app_0);
+
+    let t = do_type_infer(default_env, &abs_1);
+    println!("{}", t.to_string());
+}
+
+// f: x,y -> x(y(1))(y(1))
+fn test_three(default_env:& Env) {
+    let y_1 = Exp::EVar("y".to_string());
+    let p_0 = Exp::ELit(Literal::LInt(1));
+    let app_0 = Exp::EApp(&y_1, &p_0);
+
+    let x_1 = Exp::EVar("x".to_string());
+    let app_1 = Exp::EApp(&x_1, &app_0);
+
+    let y_2 = Exp::EVar("y".to_string());
+    let p_1 = Exp::ELit(Literal::LInt(1));
+    let app_2 = Exp::EApp(&y_2, &p_1);
+
+    let app_3 = Exp::EApp(&app_1, &app_2);
+
+    let abs_1 = Exp::EAbs("y".to_string(), &app_3);
+    let abs_2 = Exp::EAbs("x".to_string(), &abs_1);
+
+    let t = do_type_infer(default_env, &abs_2);
+    println!("{}", t.to_string());
+}
+
+// let id = x -> x in id id
+fn test_four(default_env:& Env) {
     let e1_return_exp = Exp::EVar("x".to_string());
     let e1 = Exp::EAbs("x".to_string(), &e1_return_exp);
 
@@ -33,25 +76,8 @@ fn test_let_id_x_x_id(default_env:& Env) {
     println!("{}", t.to_string());
 }
 
-fn test_let_id_x_x_id_2(default_env:& Env) {
-    let e1_return_exp = Exp::EVar("x".to_string());
-    let e1 = Exp::EAbs("x".to_string(), &e1_return_exp);
-
-    let f_1 = Exp::EVar("id".to_string());
-    let f_2 =  Exp::EVar("id".to_string());
-    let e2 = Exp::EApp(&f_1, &f_2);
-
-    let two = Exp::ELit(Literal::LInt(2));
-    let e2_invoke = Exp::EApp(&e2, &two);
-
-    let let_exp = Exp::ELet("id".to_string(), &e1, &e2_invoke);
-    
-    let t = do_type_infer(default_env, &let_exp);
-    println!("{}", t.to_string());
-}
-
-fn test_let_id_x_let_y_x_x_id(default_env:& Env) {
-
+// let id = let y = x -> y in id
+fn test_five(default_env:& Env) {
     let e1_inner = Exp::EVar("x".to_string());
     let e2_inner = Exp::EVar("y".to_string());
     let let_exp_inner = Exp::ELet("y".to_string(), &e1_inner, &e2_inner);
@@ -64,21 +90,4 @@ fn test_let_id_x_let_y_x_x_id(default_env:& Env) {
     
     let t = do_type_infer(default_env, &let_exp);
     println!("{}", t.to_string());
-}
-
-fn test_abs(default_env:& Env) {
-    // let e1_inner = Exp::EVar("x".to_string());
-    // let e2_inner = Exp::EVar("y".to_string());
-    // let let_exp_inner = Exp::ELet("y".to_string(), &e1_inner, &e2_inner);
-
-    // let e1 = Exp::EAbs("x".to_string(), &let_exp_inner);
-
-    // let e2 = Exp::EVar("id".to_string());
-
-    // let let_exp = Exp::ELet("id".to_string(), &e1, &e2);
-
-    // let abs = Exp::EApp("x".to_string(), )
-    
-    // let t = do_type_infer(default_env, &let_exp);
-    // println!("{}", t.to_string());
 }
